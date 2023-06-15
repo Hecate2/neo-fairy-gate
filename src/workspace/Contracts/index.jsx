516af09e0d8dbb91bb75d12fbaaf3d5ede0cffb5
Hecate2 <2474101468@qq.com>
2023-06-15 13:33:35 +0800

    saveToStorage; importFromUpload

--- src/workspace/Contracts/index.jsx
+++ src/workspace/Contracts/index.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import { ImportOutlined, UploadOutlined } from "@ant-design/icons";
 import { FairyClient } from "../../libs/NeoFairyClient";
+import { Hash160Str } from "../../libs/types";
 import "./index.css"
 
 class Upload extends Component{
@@ -25,7 +26,7 @@ class Upload extends Component{
   importFromRpcUrl(url){
       let contracts = [...this.contractAddressInput.current.value.matchAll(new RegExp(/0[xX][0-9a-fA-F]{40}/g))];
       let client = new FairyClient(url);
-      client.metaRpcMethod("")
+      client.invokefunction_of_any_contract()
   }
   importFromUpload(f){
     // let files = [f.target.files[0], f.target.files[1], f.target.files[2], f.target.files[3]];
@@ -56,14 +57,19 @@ class Upload extends Component{
     Promise.resolve().then(() => {
       if (manifest !== undefined){
         let reader = new FileReader();  reader.onload = (e) => {
-          args.manifest = e.target.result;
+            args.manifest = e.target.result;
+            var json = JSON.parse(args.manifest);
+            args.name = json["name"];
         };
         reader.readAsText(manifest);
       }
     }).then(() => {
       if (nef !== undefined){
         let reader = new FileReader();  reader.onload = (e) => {
-          args.nef = e.target.result;
+            args.nef = window.btoa(  // base64
+                new Uint8Array(e.target.result)
+                    .reduce((data, byte) => data + String.fromCharCode(byte), '')
+            );
         };
         reader.readAsArrayBuffer(nef);
       }
@@ -77,98 +83,116 @@ class Upload extends Component{
     }).then(() => {
       if (nefdbgnfo !== undefined){
         let reader = new FileReader();  reader.onload = (e) => {
-          args.nefdbgnfo = e.target.result;
-        };
-        reader.readAsArrayBuffer(nefdbgnfo);
+            args.nefdbgnfo = window.btoa(  // base64
+                new Uint8Array(e.target.result)
+                    .reduce((data, byte) => data + String.fromCharCode(byte), '')
+            );
+          };
+          reader.readAsArrayBuffer(nefdbgnfo);
       }
     }).then(() => {
-      // todo: name, ... in args
-      console.log(args);
-      new SingleContract(args);
+        // todo: name, ... in args
+        SingleContract.saveToStorage(args);
     });
   }
 
   render() {
     return (
-      <div className="Upload">
-        <div>
-          <label className="ContractMultiFileUpload">
-            <span className="UploadButton">
-              <input type="file" multiple="multiple" accept=".nef,.nef.txt,.manifest.json,.nefdbgnfo" onChange={this.importFromUpload}/>
-              {/*<input type="file" multiple="multiple" webkitdirectory=""/>*/}
-              <UploadOutlined /> UPLOAD FILES
-            </span>
-          </label>
-        </div>
-        [.nef, .manifest.json, dumpnef(.nef.txt), .nefdbgnfo]
-        <div>
-          Or IMPORT these contracts...
-          <div>
-            <textarea id="contractAddressInput" placeholder="0[xX][0-9a-fA-F]{40}" ref={this.contractAddressInput}
-                      style={{width: "336px", height: "28px", alignSelf: "center"}}/>
+            <div className="Upload">
+            <div>
+              <label className="ContractMultiFileUpload">
+                <span className="UploadButton">
+                  <input type="file" multiple="multiple" accept=".nef,.nef.txt,.manifest.json,.nefdbgnfo" onChange={this.importFromUpload}/>
+                  {/*<input type="file" multiple="multiple" webkitdirectory=""/>*/}
+                  <UploadOutlined /> UPLOAD FILES
+                </span>
+              </label>
+            </div>
+            [.nef, .manifest.json, dumpnef(.nef.txt), .nefdbgnfo]
+            <div>
+              Or IMPORT these contracts...
+              <div>
+                <textarea id="contractAddressInput" placeholder="0[xX][0-9a-fA-F]{40}" ref={this.contractAddressInput}
+                          style={{width: "336px", height: "28px", alignSelf: "center"}}/>
+              </div>
+            </div>
+            <label className="ContractImport">
+              <span className="UploadButton">
+                <button onClick={this.importFromActive}/>
+                <ImportOutlined /> FROM ACTIVE
+              </span>
+            </label>
+            <label className="ContractImport">
+              <span className="UploadButton">
+                <button onClick={this.importFromStandby}/>
+                <ImportOutlined /> FROM STANDBY
+              </span>
+            </label>
           </div>
-        </div>
-        <label className="ContractImport">
-          <span className="UploadButton">
-            <button onClick={this.importFromActive}/>
-            <ImportOutlined /> FROM ACTIVE
-          </span>
-        </label>
-        <label className="ContractImport">
-          <span className="UploadButton">
-            <button onClick={this.importFromStandby}/>
-            <ImportOutlined /> FROM STANDBY
-          </span>
-        </label>
-      </div>
     );
   }
 }
 
 class SingleContract extends Component{
-  constructor(props) {
-    super(props);
-    this.name = props.name;
-    this.scriptHash = props.scriptHash;
-    this.nef = props.nef;  // Buffer
-    this.manifest = props.manifest;
-    this.dumpnef = props.dumpnef;
-    this.nefdbgnfo = props.nefdbgnfo;  // Buffer
-    this.saveToStorage = this.saveToStorage.bind(this);
-  }
+    constructor(props) {
+        super(props);
 
-  saveToStorage(){
-    localStorage.setItem(this.scriptHash, JSON.stringify({
-      "name": this.name,
-      "scriptHash": this.scriptHash,
-      "nef": this.nef.toString('base64'),
-      "manifest": this.manifest,
-      "dumpnef": this.dumpnef,
-      "nefdbgnfo": this.nefdbgnfo.toString('base64'),
-    }))
-  }
+    //    this.name = props.name;
+    //    this.scriptHash = props.scriptHash;
+    //    this.nef = props.nef;  // base64 string
+    //    this.manifest = props.manifest;
+    //    this.dumpnef = props.dumpnef;
+    //    this.nefdbgnfo = props.nefdbgnfo;  // base64 string
+    }
 
-  loadFromStorage(scriptHash){
-    let contract = localStorage.getItem(this.scriptHash);
-    // todo
-  }
+    static saveToStorage(props) {
+        if (typeof (props.name) !== "string") throw "Invalid contract name!";
+        let fairyContracts = JSON.parse(localStorage.getItem("fairyContracts"));
+        if (fairyContracts === null || fairyContracts === undefined)
+            fairyContracts = {};
+        delete fairyContracts.undefined;
+        let oldContract = fairyContracts[props.name];
+        if (oldContract === null || oldContract === undefined)
+            oldContract = {};
+        oldContract.name = props.name;
+        oldContract.scriptHash = props.scriptHash ?? oldContract.scriptHash;
+        oldContract.nef = props.nef ?? oldContract.nef;
+        oldContract.manifest = props.manifest ?? oldContract.manifest;
+        oldContract.dumpnef = props.dumpnef ?? oldContract.dumpnef;
+        oldContract.nefdbgnfo = props.nefdbgnfo ?? oldContract.nefdbgnfo;
+        fairyContracts[props.name] = oldContract;
+        localStorage.setItem("fairyContracts", JSON.stringify(fairyContracts));
+        console.log(`Saved contract ${props.name}:`, fairyContracts[props.name]);
+    }
+
+    static loadFromStorage(name) {
+        let contractJson = JSON.parse(localStorage.getItem("fairyContracts"))[name];
+        return new SingleContract(contractJson)
+    }
 
   render() {
     return(
         <div className="SingleContract">
-
+            {this.name}
         </div>
     )
   }
 }
 
 class ManageContracts extends Component{
-  render() {
-    return (
-        <div className="ManageContracts" id="manageContracts">
+    static allContracts() {
+        return Object.values(JSON.parse(localStorage["fairyContracts"])).map(v => new SingleContract(v));
+    }
 
-        </div>
-    );
+    render() {
+        const contracts = ManageContracts.allContracts();
+        return (
+            <div className="ManageContracts" id="manageContracts">Contracts
+                {contracts.map(({ name, scriptHash, nef }) => (
+                    <p>{name}</p>
+                ))}
+            </div>
+        );
   }
 }
 
